ac80fa26036017e7de09e5ae0faa11a9b83f54ce
ralph_the_badger <badger_rr@gmx.de>
2019-09-17 22:08:05 +0200

    Basic structure with comments

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
+// require Packages
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
+// make functionality of express available
 const app = express();
 
+// define EJS as view engine and where to find EJS-views
 app.set("view engine", "ejs");
 app.set("views", "views");
 
+// use body parser to get request data in body
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// define path to static folder "public"
 app.use(express.static(path.join(__dirname, "public")));
 
+// require and use connection to route
 const adminRoutes = require("./routes/admin");
-
 app.use(adminRoutes);
 
+// listen to localhost port 3000
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,13 @@
-const path = require("path");
+// require express
 const express = require("express");
 
+// use Router()-method of express
 const router = express.Router();
 
+// use router functionality to get homepage and to render the view home.ejs
 router.get("/", (req, res, next) => {
   res.render("home");
 });
 
+// export router functionality
 module.exports = router;
